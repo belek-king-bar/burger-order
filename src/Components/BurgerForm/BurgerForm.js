4077d2f0bc505bb4f78da31393208023dd3b4510
belek <belektolubaev@gmail.com>
2019-02-07 19:40:55 +0600

    Стили

--- src/Components/BurgerForm/BurgerForm.js
+++ src/Components/BurgerForm/BurgerForm.js
@@ -8,7 +8,7 @@ function BurgerForm(props) {
     return (
         <Card body inverse color="danger" className="container text-center mb-4 mt-4">
             <h4 className="mb-4">Цена: {props.total} </h4>
-            <div>
+            <div className="m-0">
 
                 {availableIngredients.map(item => {
                     return <IngredientsForm
--- src/Components/BurgerForm/IngredientsForm/IngredientsForm.js
+++ src/Components/BurgerForm/IngredientsForm/IngredientsForm.js
@@ -6,7 +6,7 @@ function IngredientsForm(props) {
             <Col className="text-right">
                 <h4>{props.label}</h4>
             </Col>
-            <Col className="m-0 mb-2">
+            <Col className="m-0 mb-2" xs={2}>
                 <Button disabled={props.isAddButtonDisabled()} type="submit" onClick={props.onChangeIngredient}>LESS</Button>
             </Col>
             <Col className="text-left m-0 mb-2">
